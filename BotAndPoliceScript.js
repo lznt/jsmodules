a9a7f1c6e736e0d4e7a0cb35510c33eef1298727
lznt <lasseann@CSE-PC59.(none)>
2012-07-18 12:38:49 +0300

    Added botandpolicescript.js

---
+++ BotAndPoliceScript.js
@@ -0,0 +1,60 @@
+function BotAndPolice(entity, comp) {
+	this.me = entity;
+	frame.Updated.connect(this, this.Update);
+	this.busted = false;
+}
+
+function printObj(obj) {
+  for (i in obj) {
+    if (i == "locale") // This is a hack to avoid calling QLocale::toString in iteration, which would throw an overload resolution error inside Qt.
+      print(i + ": locale"); 
+    else
+      print(i + ": " + obj[i]);
+  }
+}     
+
+
+
+BotAndPolice.prototype.IfCloseTo = function(frametime){
+	for(i in scriptObjects){
+		if (scriptObjects[i].me == null){
+			print('Entity removed or not initialized yet');
+		
+		}else{
+		
+			//print (scriptObjects[i].me);
+			var pos = this.me.placeable.Position();
+			var yNow = pos.y;
+			var xNow = pos.x;
+			var zNow = pos.z;
+			
+			var dist = Math.sqrt(Math.pow((xNow - scriptObjects[i].me.placeable.Position().x), 2) + 
+				Math.pow((zNow - scriptObjects[i].me.placeable.Position().x), 2));
+			
+			print (dist);
+			//Here we can separate PoliceMan from Another player
+			if(dist <= 25 && this.me.Id() != scriptObjects[i].me.Id()){
+				this.busted = true;
+				print('Busted in da hood');
+				this.me.dynamiccomponent.SetAttribute('ifToWalk', false);
+						
+			}else{
+				this.me.dynamiccomponent.SetAttribute('ifToWalk', true);
+				this.busted = false;
+				//this.me.dynamiccomponent.SetAttribute('ifToWalk', true);
+			
+		}
+	}
+
+}
+
+
+BotAndPolice.prototype.Update = function(frametime) {
+	if (server.IsRunning())
+			this.IfCloseTo(frametime);
+			
+		else
+			this.IfCloseTo(frametime);
+
+		
+}
