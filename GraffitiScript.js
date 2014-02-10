72c2c12de0e27dd2456ddeafe7174c8ae6926a5a
lznt <lasseann@ee.oulu.fi>
2014-02-10 11:31:08 +0200

    Added spraying

--- GraffitiScript.js
+++ GraffitiScript.js
@@ -1,4 +1,4 @@
-//All planes have to have this script to monitor the current graffiti logo.
+//All planes have to have this script to monitor it.
 
 // Include the json parse/stringify library. We host it here if you want to use it:
 // !ref: http://meshmoon.data.s3.amazonaws.com/app/lib/json2.js, Script
@@ -11,8 +11,9 @@ engine.IncludeFile("http://meshmoon.data.s3.amazonaws.com/app/lib/class.js");
 engine.IncludeFile("http://meshmoon.data.s3.amazonaws.com/app/lib/admino-utils-common-deploy.js");
 SetLogChannelName("GraffitiScript"); //this can be anything, but best is your aplication name
 
+//Links for graffitimaterials.
 var graffitiLinks = ["http://meshmoon.eu.scenes.2.s3.amazonaws.com/mediateam-b4527d/test2/graffiti/blue.material",
-   "http://meshmoon.eu.scenes.2.s3.amazonaws.com/mediateam-b4527d/test2/graffiti/purple.material", "http://meshmoon.eu.scenes.2.s3.amazonaws.com/mediateam-b4527d/test2/graffiti/green.material"];
+"http://meshmoon.eu.scenes.2.s3.amazonaws.com/mediateam-b4527d/test2/graffiti/purple.material", "http://meshmoon.eu.scenes.2.s3.amazonaws.com/mediateam-b4527d/test2/graffiti/green.material"];
 var scene = me.ParentScene();
 //Interval so server is queried only once every 2seconds. (Roughly)
 var interval = 0;
@@ -24,59 +25,62 @@ Log("Script starting...");
 frame.Updated.connect(Update);
 
 var myHandler = function (myAsset) {
-    Log("Successfully connected");
-    //Check if data on server has changed or not. No need to parse same data over and over again.
-    if (String(myAsset.RawData()) == currentData) {
-    	    Log("Nothing changed on serv.")
-    	    return;
-    } else
-    	currentData = String(myAsset.RawData());
-    
-    var data = JSON.parse(myAsset.RawData());
-    for(var i=0; i<data.length; ++i) {
-          checkVenue(data[i]);
-    }
-    
-    // Forget the disk asset so it wont be returned from cache next time you do the same request
-    asset.ForgetAsset(myAsset.Name(), true);
+	Log("Successfully connected");
+	//Check if data on server has changed or not. No need to parse same data over and over again.
+	if (String(myAsset.RawData()) == currentData) {
+		Log("Nothing changed on serv.")
+		return;
+	} else
+    		currentData = String(myAsset.RawData());
+    	
+    	var data = JSON.parse(myAsset.RawData());
+    	for(var i=0; i<data.length; ++i) {
+    		checkVenue(data[i]);
+    	}
+    	// Forget the disk asset so it wont be returned from cache next time you do the same request
+    	asset.ForgetAsset(myAsset.Name(), true);
 }
 
-
-
-//Simple logic to check which gang owns which plane 
+//Simple logic to check which gang owns which plane and if spraying is enabled.
 function checkVenue (venueData) {
-  var venueName = venueData.name;
-  var venueGang = venueData.gang;
-  //Syntax for graffitis
-  var plane = scene.EntityByName("graffiti-plane-" + venueName);
-
-  if (venueGang == "Blue Angels" && plane) {
-    plane.mesh.materialRefs = new Array(graffitiLinks[0]);
-    Log("Found something by Blue Angels");
-  } else if (venueGang == "Purple Knights" && plane) {
-    plane.mesh.materialRefs = new Array(graffitiLinks[1]);
-    Log("Found something by Purple Knights");
-  } else if (venueGang == "Green Shamans" && plane) {
-    plane.mesh.materialRefs = new Array(graffitiLinks[2]);
-    Log("Found something by Green Shamans");
-  }
+	var venueName = venueData.name;
+	var venueGang = venueData.gang;
+	
+	//Syntax for graffitis
+	var plane = scene.EntityByName("graffiti-plane-" + venueName);
+	//Check venue owner
+	if (venueGang == "Blue Angels" && plane) {
+		plane.mesh.materialRefs = new Array(graffitiLinks[0]);
+		Log("Found something by Blue Angels");
+	} else if (venueGang == "Purple Knights" && plane) {
+		plane.mesh.materialRefs = new Array(graffitiLinks[1]);
+		Log("Found something by Purple Knights");
+	} else if (venueGang == "Green Shamans" && plane) {
+		plane.mesh.materialRefs = new Array(graffitiLinks[2]);
+		Log("Found something by Green Shamans");
+	}
+	
+	//Put spraying particle on, if venue is spraying.
+	var venueSpraying = venueData.sprayinginitialized;
+	if (venueSpraying == 1)
+		plane.particlesystem.enabled = true;
+	else
+		plane.particlesystem.enabled = false;
+	
 }
 
 //Change later to work serverside
 function Update () {
-  if (!server.IsRunning()) {
-      //GET venues    
-      //Query server only every 2seconds ("roughly")
-      if (interval > 50) {
-        var transfer = asset.RequestAsset("http://vm0063.virtues.fi/venues/?active", "Binary", true);
-        transfer.Succeeded.connect(myHandler);
-        interval = 0;
-      } else
-        interval ++;
-  } else {
-        Log("This is the server...");
-  }
+	if (!server.IsRunning()) {
+		//GET venues    
+		//Query server only every 2seconds ("roughly")
+		if (interval > 50) {
+			var transfer = asset.RequestAsset("http://vm0063.virtues.fi/venues/?active", "Binary", true);
+			transfer.Succeeded.connect(myHandler);
+			interval = 0;
+		} else
+			interval ++;
+	} else {
+		Log("This is the client...");
+	}
 }
-
-
-
